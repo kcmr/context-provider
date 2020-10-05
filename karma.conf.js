044a5a13865aeee966bf3fa5c52298defd7940b6
Kus Cámara <kcmr@users.noreply.github.com>
2020-10-05 19:26:33 +0200

    ci: fix coverage reports (#3)
    * docs(readme): fix typo in development command
    
    * test: back to testing-karma
    
    web-test-runner does not generate lcov.info and seems unstable

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+const { createDefaultConfig } = require('@open-wc/testing-karma');
+const { merge } = require('webpack-merge');
+
+module.exports = (config) =>
+  config.set(
+    merge(createDefaultConfig(config), {
+      files: [
+        {
+          pattern: config.grep ? config.grep : 'test/**/*.test.js',
+          type: 'module',
+        },
+      ],
+
+      client: {
+        mocha: {
+          ui: 'tdd',
+        },
+      },
+
+      coverageReporter: {
+        check: {
+          global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+          },
+        },
+      },
+
+      esm: {
+        nodeResolve: true,
+      },
+    }),
+  );
